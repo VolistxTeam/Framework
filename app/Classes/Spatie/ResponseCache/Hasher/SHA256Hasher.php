aa0f9fc8641510b0dc8cdc77a6df81d62479b46d
Cryental <contact@cryental.com>
2021-10-07 21:42:20 +0600

    Fixed Bugs with Custom Caching

--- app/Classes/Spatie/ResponseCache/Hasher/SHA256Hasher.php
+++ app/Classes/Spatie/ResponseCache/Hasher/SHA256Hasher.php
@@ -3,10 +3,16 @@
 namespace App\Classes\Spatie\ResponseCache\Hasher;
 
 use Illuminate\Http\Request;
+use Spatie\ResponseCache\CacheProfiles\CacheProfile;
 use Spatie\ResponseCache\Hasher\RequestHasher;
 
 class SHA256Hasher implements RequestHasher
 {
+    public function __construct(protected CacheProfile $cacheProfile)
+    {
+        //
+    }
+
     public function getHashFor(Request $request): string
     {
         return 'volistx-caching:' . hash('sha256', "{$request->getHost()}-{$request->getRequestUri()}-{$request->getMethod()}/" . $this->cacheProfile->useCacheNameSuffix($request));
