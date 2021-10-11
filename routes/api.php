452db0fd190c81a327928bfff078cb7f60c38068
Cryental <contact@cryental.dev>
2021-10-11 19:30:45 +0600

    Updated Proxy IP Blocks

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,6 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
-
 $router->group(['middleware' => ['auth.user', 'cacheResponse:5']], function () use ($router) {
     $router->get('/example', 'Services\ExampleService@Example');
 });
