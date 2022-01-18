c2691cdc0ba0453730fc6bf8b18c2be80578a6ba
Scorias <mostafa.samy203@gmail.com>
2022-01-18 09:33:28 +0200

    adding LogRepository.php extra functions

--- app/Repositories/LogRepository.php
+++ app/Repositories/LogRepository.php
@@ -20,12 +20,12 @@ class LogRepository
         ]);
     }
 
-    public function Find($personal_token_id, $log_id)
+    public function FindById($personal_token_id, $log_id)
     {
         return Log::query()->where('id', $log_id)->where('personal_token_id', $personal_token_id)->first();
     }
 
-    public function FindAll($personal_token_id,$needle,$page,$limit)
+    public function FindLogsByToken($personal_token_id,$needle,$page,$limit)
     {
         $columns = Schema::getColumnListing('logs');
         $query = Log::query();
@@ -33,6 +33,38 @@ class LogRepository
         foreach($columns as $column) {
             $query->orWhere("logs.$column", 'LIKE', "%$needle%");
         }
-        return $query->where('personal_token_id', $personal_token_id)->paginate($limit, ['*'], 'page', $page);
+        return $query->where('personal_token_id', $personal_token_id)
+            ->orderBy('created_at', 'DESC')
+            ->paginate($limit, ['*'], 'page', $page);
+    }
+
+    public function FindLogsByTokenCount($personal_token_id,Carbon $date): int
+    {
+        return Log::query()->where('personal_token_id',$personal_token_id)
+            ->whereMonth('created_at', $date->format('m'))
+            ->whereYear('created_at', $date->format('Y'))            ->count();
+    }
+
+    public function FindLogsBySubscription($subscription_id,$needle,$page,$limit)
+    {
+        $columns = Schema::getColumnListing('logs');
+        $query = Log::query();
+
+        foreach($columns as $column) {
+            $query->orWhere("logs.$column", 'LIKE', "%$needle%");
+        }
+        return $query->join('personal_tokens', 'personal_tokens.id', '=', 'logs.personal_token_id')
+            ->where('personal_tokens.subscription_id', $subscription_id)
+            ->orderBy('created_at', 'DESC')
+            ->paginate($limit, ['*'], 'page', $page);
+    }
+
+    public function FindLogsBySubscriptionCount($subscription_id, $date): int
+    {
+        return Log::query()->join('personal_tokens', 'personal_tokens.id', '=', 'logs.personal_token_id')
+            ->where('personal_tokens.subscription_id', $subscription_id)
+            ->whereMonth('created_at', $date->format('m'))
+            ->whereYear('created_at', $date->format('Y'))
+            ->count();
     }
 }
