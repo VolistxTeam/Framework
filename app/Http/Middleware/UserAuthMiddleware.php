da607b726687c3022ddd4970c2711e7e45e32fca
Scorias <mostafa.samy203@gmail.com>
2022-01-20 15:27:58 +0200

    update the auth middleware to attach the keys to request

--- app/Http/Middleware/UserAuthMiddleware.php
+++ app/Http/Middleware/UserAuthMiddleware.php
@@ -4,8 +4,9 @@ namespace App\Http\Middleware;
 
 use App\Classes\MessagesCenter;
 use App\Classes\PermissionsCenter;
-use App\Models\Log;
-use App\Repositories\LogRepository;
+use App\Models\AdminLog;
+use App\Repositories\AdminLogRepository;
+use App\Repositories\UserLogRepository;
 use Carbon\Carbon;
 use Closure;
 use Illuminate\Http\Request;
@@ -14,8 +15,8 @@ use Wikimedia\IPSet;
 
 class UserAuthMiddleware
 {
-    private LogRepository $logRepository;
-    public function __construct(LogRepository $logRepository)
+    private UserLogRepository $logRepository;
+    public function __construct(UserLogRepository $logRepository)
     {
         $this->logRepository= $logRepository;
     }
@@ -27,32 +28,21 @@ class UserAuthMiddleware
         if (!$key || ($key->expires_at && Carbon::now()->greaterThan(Carbon::createFromTimeString($key->expires_at)))) {
             return response()->json(MessagesCenter::E403(), 403);
         }
+//TODO: IP CHECKS
+//        $ipSet = new IPSet($key->whitelist_range);
+//        if (!empty($key->whitelist_range) && !$ipSet->match($request->getClientIp())) {
+//            return response()->json(MessagesCenter::E403("Not allowed in your location"), 403);
+//        }
 
-        $ipSet = new IPSet($key->whitelist_range);
-        if (!empty($personalKeys->whitelist_range) && !$ipSet->match($request->getClientIp())) {
-            return response()->json(MessagesCenter::E403(), 403);
-        }
+//TODO: REQUESTS CHECKS
+//        $requestsMadeCount = $this->logRepository->FindLogsBySubscriptionCount($key->subscription()->get->id,Carbon::now());
+//        $planRequestsLimit = $key->subscription()->get()->plan()->get()->requests;
+//        if ($planRequestsLimit != -1 && $requestsMadeCount >= $planRequestsLimit) {
+//            return response()->json(MessagesCenter::E429(), 429);
+//        }
 
-
-        $requestsMadeCount = $this->logRepository->FindLogsBySubscriptionCount($key->subscription()->get->id,Carbon::now());
-        $planRequestsLimit = $key->subscription()->get()->plan()->get()->requests;
-        if ($planRequestsLimit != -1 && $requestsMadeCount >= $planRequestsLimit) {
-            return response()->json(MessagesCenter::E429(), 429);
-        }
-
-        //TO CHECK HOW TO WRITE LOGS LATER WITH CRYENTAL
-        $log = [
-            'url' => $request->getUri(),
-            'method' => $request->getMethod(),
-            'headers' => $request->headers->all(),
-            'body' => $request->all()
-        ];
-
-        $this->logRepository->Create($key->id,[
-            'personal_token_id' => $key->id,
-            'key' => "key",
-            'value' =>"value",
-            'type'=>"type"
+        $request->merge([
+            'X-PERSONAL-TOKEN'=>$key,
         ]);
 
       return $next($request);
