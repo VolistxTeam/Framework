cefc47ce87e8060bff5e2db23397ef1e18fbb89f
Scorias <mostafa.samy203@gmail.com>
2022-01-21 12:59:31 +0200

    feat: adding validation structure

---
+++ app/Classes/ValidationRules/IPValidationRule.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Classes\ValidationRules;
+
+use App\Classes\MessagesCenter;
+use Wikimedia\IPSet;
+
+class IPValidationRule extends ValidationRuleBase
+{
+    public function Validate(): bool|array
+    {
+        $token = $this->inputs['token'];
+        $request = $this->inputs['request'];
+
+        $ipSet = new IPSet(json_decode($token->whitelist_range));
+        if (!empty($token->whitelist_range) && !$ipSet->match($request->getClientIp())) {
+            return [
+                'message'=>MessagesCenter::E403("Not allowed in your location"),
+                'code' => 403
+            ];
+        }
+        return true;
+    }
+}
---
+++ app/Classes/ValidationRules/RequestsCountValidationRule.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Classes\ValidationRules;
+
+use App\Classes\MessagesCenter;
+use App\Repositories\UserLogRepository;
+use Carbon\Carbon;
+use Wikimedia\IPSet;
+
+class RequestsCountValidationRule extends ValidationRuleBase
+{
+    public function Validate(): bool|array
+    {
+        $token = $this->inputs['token'];
+
+        $repository = new UserLogRepository();
+        $requestsMadeCount = $repository ->FindLogsBySubscriptionCount($token->subscription()->first()->id,Carbon::now());
+        $planRequestsLimit = $token->subscription()->first()->plan()->first()->requests;
+        if ($planRequestsLimit != -1 && $requestsMadeCount >= $planRequestsLimit) {
+            return [
+                'message' => MessagesCenter::E429(),
+                'code' => 429
+            ];
+        }
+        return true;
+    }
+}
