88f4580ef3b01e2d40335676bcad2ddc772d8c5c
Scorias <mostafa.samy203@gmail.com>
2022-01-21 13:00:10 +0200

    refactor: UserAuthMiddleware.php to use validation rules

--- app/Http/Middleware/UserAuthMiddleware.php
+++ app/Http/Middleware/UserAuthMiddleware.php
@@ -4,45 +4,45 @@ namespace App\Http\Middleware;
 
 use App\Classes\MessagesCenter;
 use App\Classes\PermissionsCenter;
-use App\Models\AdminLog;
-use App\Repositories\AdminLogRepository;
-use App\Repositories\UserLogRepository;
+use App\Classes\ValidationRules\IPValidationRule;
+use App\Classes\ValidationRules\KeyValidationRule;
+use App\Classes\ValidationRules\RequestsCountValidationRule;
+use App\Classes\ValidationRules\ValidationRuleBase;
 use Carbon\Carbon;
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Str;
-use Wikimedia\IPSet;
+use JetBrains\PhpStorm\ArrayShape;
 
 class UserAuthMiddleware
 {
-    private UserLogRepository $logRepository;
-    public function __construct(UserLogRepository $logRepository)
-    {
-        $this->logRepository= $logRepository;
-    }
-
     public function handle(Request $request, Closure $next)
     {
         $key = PermissionsCenter::getUserAuthKey($request->bearerToken());
 
-        if (!$key || ($key->expires_at && Carbon::now()->greaterThan(Carbon::createFromTimeString($key->expires_at)))) {
-            return response()->json(MessagesCenter::E403(), 403);
-        }
-//TODO: IP CHECKS
-//        $ipSet = new IPSet($key->whitelist_range);
-//        if (!empty($key->whitelist_range) && !$ipSet->match($request->getClientIp())) {
-//            return response()->json(MessagesCenter::E403("Not allowed in your location"), 403);
-//        }
 
-//TODO: REQUESTS CHECKS
-//        $requestsMadeCount = $this->logRepository->FindLogsBySubscriptionCount($key->subscription()->get->id,Carbon::now());
-//        $planRequestsLimit = $key->subscription()->get()->plan()->get()->requests;
-//        if ($planRequestsLimit != -1 && $requestsMadeCount >= $planRequestsLimit) {
-//            return response()->json(MessagesCenter::E429(), 429);
-//        }
+        //prepare inputs array
+        $inputs = [
+            'request' => $request,
+            'token' => $key,
+        ];
+
+        //add extra validators in the required order.
+        //To be refactored to detect all classes with a base of ValidationRuleBase and create instance of them passing parameters, and ordering them by id
+        $validators = [
+            new KeyValidationRule($inputs),
+            new IPValidationRule($inputs),
+            new RequestsCountValidationRule($inputs)
+        ];
+
+        foreach ($validators as $validator){
+            $result = $validator->validate();
+            if($result !== true){
+                return response()->json($result['message'],$result['code']);
+            }
+        }
 
         $request->merge([
-            'X-PERSONAL-TOKEN'=>$key,
+            'X-PERSONAL-TOKEN'=> $key,
         ]);
 
       return $next($request);
