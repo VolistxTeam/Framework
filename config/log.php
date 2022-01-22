272e6441010acba93f74e24ba963a8143b3d04cc
Scorias <mostafa.samy203@gmail.com>
2022-01-22 15:56:35 +0200

    finalize logging system

---
+++ config/log.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'adminLogMode' => 'local',
+    'adminLogHttpUrl' => "",
+    'adminLogHttpToken' =>"",
+    'userLogMode' => 'local',
+    'userLogHttpUrl' => "",
+    'userLogHttpToken'=>"",
+];
