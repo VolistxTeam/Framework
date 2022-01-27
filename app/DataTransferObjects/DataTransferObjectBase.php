bd6adb6a0c61708faaae050610d06ae03b519da7
Scorias <mostafa.samy203@gmail.com>
2022-01-27 14:13:53 +0200

    feat: adding logic for DTO base

---
+++ app/DataTransferObjects/DataTransferObjectBase.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\DataTransferObjects;
+
+use ReflectionClass;
+use ReflectionProperty;
+
+abstract class DataTransferObjectBase
+{
+
+    public function __construct(array $parameters = [])
+    {
+        $class = new ReflectionClass(static::class);
+
+        foreach ($class->getProperties(ReflectionProperty::IS_PUBLIC) as $reflectionProperty){
+            $property = $reflectionProperty->getName();
+            $this->{$property} = $parameters[$property];
+        }
+    }
+
+}
