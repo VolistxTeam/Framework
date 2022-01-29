6693cdfef0ac766a0a7c554f385b0127100b34d5
Scorias <mostafa.samy203@gmail.com>
2022-01-29 15:25:32 +0200

    refactor : UserAuthMiddleware.php

--- app/Http/Middleware/UserAuthMiddleware.php
+++ app/Http/Middleware/UserAuthMiddleware.php
@@ -2,30 +2,39 @@
 
 namespace App\Http\Middleware;
 
-use App\Classes\PermissionsCenter;
 use App\Classes\ValidationRules\IPValidationRule;
-use App\Classes\ValidationRules\KeyValidationRule;
+use App\Classes\ValidationRules\KeyExpiryValidationRule;
 use App\Classes\ValidationRules\RateLimitValidationRule;
 use App\Classes\ValidationRules\RequestsCountValidationRule;
+use App\Classes\ValidationRules\ValidKeyValidationRule;
+use App\Repositories\PersonalTokenRepository;
 use Closure;
 use Illuminate\Http\Request;
 
 class UserAuthMiddleware
 {
+    private PersonalTokenRepository $personalTokenRepository;
+    public function __construct(PersonalTokenRepository $personalTokenRepository)
+    {
+        $this->personalTokenRepository = $personalTokenRepository;
+    }
+
+
     public function handle(Request $request, Closure $next)
     {
-        $key = PermissionsCenter::getUserAuthKey($request->bearerToken());
+        $token = $this->personalTokenRepository->AuthPersonalToken($request->bearerToken());
 
         //prepare inputs array
         $inputs = [
             'request' => $request,
-            'token' => $key,
+            'token' => $token,
         ];
 
         //add extra validators in the required order.
         //To be refactored to detect all classes with a base of ValidationRuleBase and create instance of them passing parameters, and ordering them by id
         $validators = [
-            new KeyValidationRule($inputs),
+            new ValidKeyValidationRule($inputs),
+            new KeyExpiryValidationRule($inputs),
             new IPValidationRule($inputs),
             new RequestsCountValidationRule($inputs),
             new RateLimitValidationRule($inputs)
@@ -39,7 +48,7 @@ class UserAuthMiddleware
         }
 
         $request->merge([
-            'X-PERSONAL-TOKEN' => $key,
+            'X-PERSONAL-TOKEN' => $token,
         ]);
 
         return $next($request);
