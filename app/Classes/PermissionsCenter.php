4c86bce001194ce5de4d31de73388cea09228446
Scorias <mostafa.samy203@gmail.com>
2022-02-01 11:49:37 +0200

    feat: add the PermissionsCenter.php permission list and change the check function for cleaner code

--- app/Classes/PermissionsCenter.php
+++ app/Classes/PermissionsCenter.php
@@ -6,18 +6,44 @@ use App\Models\AccessToken;
 use App\Models\PersonalToken;
 use Illuminate\Support\Facades\Hash;
 use Wikimedia\IPSet;
+use function PHPUnit\Framework\returnArgument;
 
 class PermissionsCenter
 {
-    public function check($key, $permissionName): bool
+    public array $Permissions =[
+        'subscriptions:*',
+        'subscriptions:create',
+        'subscriptions:update',
+        'subscriptions:delete',
+        'subscriptions:view',
+        'subscriptions:view-all',
+        'subscriptions:logs',
+
+        'personal-tokens:*',
+        'personal-tokens:create',
+        'personal-tokens:update',
+        'personal-tokens:delete',
+        'personal-tokens:reset',
+        'personal-tokens:view',
+        'personal-tokens:view-all',
+        'personal-tokens:logs',
+
+        'plans:*',
+        'plans:create',
+        'plans:update',
+        'plans:delete',
+        'plans:view',
+        'plans:view-all',
+        'plans:logs',
+
+        'logs:*',
+        'logs:view',
+        'logs:view-all',
+    ];
+
+    public function check($key, $module ,$operation, ): bool
     {
-        $permissions = $key->permissions;
-
-        if (in_array($permissionName, $permissions) || in_array('*', $permissions)) {
-            return true;
-        }
-
-        return false;
+        return in_array("$module:$operation", $key->permissions) || in_array("$module:*",$key->permissions) ;
     }
 }
 
