2cf9d41d7871ab615ec60fe6aaee176c5c61ff5e
Scorias <mostafa.samy203@gmail.com>
2022-02-01 11:52:59 +0200

    refactor: update ExampleService.php for permissions new structure

--- app/Http/Controllers/Services/ExampleService.php
+++ app/Http/Controllers/Services/ExampleService.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Controllers\Services;
 
+use App\Classes\Facades\Permissions;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use phpDocumentor\Reflection\DocBlock\Tags\Example;
 
 class ExampleService extends Controller
 {
-    public function Example(Request $request)
+    public function __construct()
     {
+        $this->module = 'example';
+    }
+
+    public function Example(Request $request): \Illuminate\Http\JsonResponse
+    {
+        if (!Permissions::check($request->X_PERSONAL_TOKEN, $this->module,'permission-name')) {
+            // do something if permissions failed
+        }
+
         return response()->json(['hehe']);
     }
 }
