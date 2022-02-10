e12c2e1cd7b1a3b23c5c00dc4200ef12f9f1f216
Scorias <mostafa.samy203@gmail.com>
2022-02-10 15:10:06 +0200

    update routes to follow REST

--- routes/system.php
+++ routes/system.php
@@ -18,22 +18,23 @@ $router->group(['prefix' => 'sys-bin'], function () use ($router) {
             $router->group(['middleware' => ['filter.json']], function () use ($router) {
                 $router->post('/', 'Auth\SubscriptionController@CreateSubscription');
                 $router->put('/{subscription_id}', 'Auth\SubscriptionController@UpdateSubscription');
+
+
+                $router->post('/{subscription_id}/personal-tokens', 'Auth\PersonalTokenController@CreatePersonalToken');
+                $router->put('/{subscription_id}/personal-tokens/{token_id}', 'Auth\PersonalTokenController@UpdatePersonalToken');
+
             });
+
             $router->delete('/{subscription_id}', 'Auth\SubscriptionController@DeleteSubscription');
             $router->get('/', 'Auth\SubscriptionController@GetSubscriptions');
             $router->get('/{subscription_id}', 'Auth\SubscriptionController@GetSubscription');
             $router->get('/{subscription_id}/logs', 'Auth\SubscriptionController@GetSubscriptionLogs');
-        });
 
-        $router->group(['prefix' => 'personal-tokens'], function () use ($router) {
-            $router->group(['middleware' => ['filter.json']], function () use ($router) {
-                $router->post('/{subscription_id}', 'Auth\PersonalTokenController@CreatePersonalToken');
-                $router->put('/{subscription_id}/{token_id}', 'Auth\PersonalTokenController@UpdatePersonalToken');
-            });
-            $router->delete('/{subscription_id}/{token_id}', 'Auth\PersonalTokenController@DeletePersonalToken');
-            $router->put('/{subscription_id}/{token_id}/reset', 'Auth\PersonalTokenController@ResetPersonalToken');
-            $router->get('/{subscription_id}/{token_id}', 'Auth\PersonalTokenController@GetPersonalToken');
-            $router->get('/{subscription_id}', 'Auth\PersonalTokenController@GetPersonalTokens');
+
+            $router->delete('/{subscription_id}/personal-tokens/{token_id}', 'Auth\PersonalTokenController@DeletePersonalToken');
+            $router->put('/{subscription_id}/personal-tokens/{token_id}/reset', 'Auth\PersonalTokenController@ResetPersonalToken');
+            $router->get('/{subscription_id}/personal-tokens/{token_id}', 'Auth\PersonalTokenController@GetPersonalToken');
+            $router->get('/{subscription_id}/personal-tokens', 'Auth\PersonalTokenController@GetPersonalTokens');
         });
 
         $router->group(['prefix' => 'plans'], function () use ($router) {
@@ -46,6 +47,7 @@ $router->group(['prefix' => 'sys-bin'], function () use ($router) {
             $router->get('/{plan_id}', 'Auth\PlanController@GetPlan');
         });
 
+        //TO DISCUSS THE LOGS HASSLE
         $router->group(['prefix' => 'logs'], function () use ($router) {
             $router->get('/', 'Auth\AdminLogController@GetAdminLogs');
             $router->get('/{log_id}', 'Auth\AdminLogController@GetAdminLog');
